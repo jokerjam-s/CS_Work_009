d0db1b8bd20981ad39b477e3c0947d244815bd8d
Андрей <40392418+jokerjam-s@users.noreply.github.com>
2022-07-05 11:08:38 +0300

    task 64, 66 ready

---
+++ Task_64/Program.cs
@@ -0,0 +1,41 @@
+﻿/**
+* * Задача 64
+*   Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1
+*
+*   N = 5 -> "5, 4, 3, 2, 1"
+*   N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*
+*/
+
+/// Получение целочисленного значения от пользователя с консоли.
+///     message - сообщение, выводимое пользователю
+int InputInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+/// Рекурсивный вывод чисел от N до 1.
+///     startNumber - стартовое число для вывода
+string GetNumbersLine(int startNumber)
+{
+    if (startNumber == 1)
+    {
+        return "1";
+    }
+
+    return startNumber.ToString() + ", " + GetNumbersLine(startNumber - 1);
+}
+
+
+int startNumber = InputInt("Input number: ");
+
+if (startNumber <= 0)
+{
+    Console.WriteLine("Number must be more than 0!");
+}
+else
+{
+    Console.WriteLine(GetNumbersLine(startNumber));
+}
+
---
+++ Task_66/Program.cs
@@ -0,0 +1,49 @@
+﻿/**
+* * Задача 66
+*   Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+*
+*   M = 1; N = 15 -> 120
+*   M = 4; N = 8. -> 30
+*
+*/
+
+/// Получение целочисленного значения от пользователя с консоли.
+///     message - сообщение, выводимое пользователю
+int InputInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+/// Рекурсивный расчет суммы чисел в заданном диапазоне. 
+///     startNumber - стартовое число
+///     endNumber - финишное число
+int GetNumbersSum(int startNumber, int endNumber)
+{
+    if (startNumber == endNumber)
+    {
+        return endNumber;
+    }
+
+    if (startNumber < endNumber)
+    {
+        return startNumber + GetNumbersSum(startNumber + 1, endNumber);
+    }
+
+    return startNumber + GetNumbersSum(startNumber - 1, endNumber);
+}
+
+
+/// Main body.
+/// Допустимы только натуральные числа
+int startNumber = InputInt("Input start number: ");
+int endNumber = InputInt("Input end number: ");
+
+if (startNumber <= 0 || endNumber <= 0)
+{
+    Console.WriteLine("You have to use only natural numbers!");
+}
+else
+{
+    Console.WriteLine(GetNumbersSum(startNumber, endNumber).ToString());
+}
